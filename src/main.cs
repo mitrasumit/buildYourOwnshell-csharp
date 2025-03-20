8b6207828b24bbfa7a23300909f817b4d5e6a6d8
Sumit Mitra <mitra.sumit@outlook.com>
2025-03-20 12:23:03 +0530

    addedType

--- src/main.cs
+++ src/main.cs
@@ -12,7 +12,16 @@ var command=Console.ReadLine();
 if(string.Equals(command,"exit 0"))
     break;
 //Writing user input
-if(command.Contains("echo"))
+if(command.Contains("type echo"))
+    Console.WriteLine("echo is a shell builtin");
+
+else if(command.Contains("type exit"))
+    Console.WriteLine("exit is a shell builtin");
+
+else if(command.Contains("type type"))
+    Console.WriteLine("type is a shell builtin");
+
+else if(command.Contains("echo"))
     Console.WriteLine($"{command.Replace("echo","").Trim()}");
 else
     Console.WriteLine($"{command}: command not found");
