e7819823b7fa37125ce5daf4b84e98ecda5a8408
Sumit Mitra <mitra.sumit@outlook.com>
2025-03-25 15:19:26 +0530

    modifiedCode

--- src/main.cs
+++ src/main.cs
@@ -1,39 +1,114 @@
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
-// Uncomment this line to pass the first stage
+string builtinPresent;
 
+bool whileFlag=true;
 
-while (true)
+while (whileFlag)
 {
-Console.Write("$ ");
-//Reading User Input
-var command=Console.ReadLine();
+    Console.Write("$ ");
 
-if(string.Equals(command,"exit 0"))
-    break;
-//Writing user input
-if(command.Contains("type echo"))
+
+    string? command = Console.ReadLine();
+
+    if (!CheckIfValidCommand(command))
+    {
+        builtinPresent = " ";
+    }
+    else
+    {
+        builtinPresent = command.TrimEnd().Split(" ")[0];
+    }
+
+    switch (builtinPresent)
+    {
+        case "exit":
+            {
+                if (command.Contains("exit 0"))
+                    whileFlag = false;
+                else
+                    Console.WriteLine($"{command}: command not found");
+                break;
+            }
+
+        case "echo":
+            {
+                Console.WriteLine(command.Replace("echo", "").Trim());
+                break;
+            }
+
+        case "type":
+            {
+                typeBuiltin(command);
+                break;
+            }
+        default:
+            {
+                customCommand(command);
+                //Console.WriteLine($"{command}: command not found");
+                break;
+            }
+    }
+}
+
+static void customCommand(string command)
+{
+    string[] paths = GetPath();
+
+    string[] commandParts=command.Split(" ");
+
+    bool foundFlag = false;
+
+
+    foreach (var path in paths)
+    {
+        var wholePath = Path.Join(path, commandParts[0]);
+
+        if (File.Exists(wholePath))
+        {
+            Process.Start(wholePath,commandParts[1]);
+            foundFlag=true;
+            break;
+        }
+    }
+
+    if (foundFlag == false)
+            Console.WriteLine($"{commandParts[0].Trim()}: not found");
+
+
+}
+
+static bool CheckIfValidCommand(string command)
+{
+    command=command.Trim();
+
+    if(string.IsNullOrEmpty(command) || string.IsNullOrWhiteSpace(command))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+static void typeBuiltin(string command)
+{
+    if(command.Contains("type echo"))
     Console.WriteLine("echo is a shell builtin");
 
-else if(command.Contains("type exit"))
+    else if(command.Contains("type exit"))
     Console.WriteLine("exit is a shell builtin");
 
-else if(command.Contains("type type"))
-    Console.WriteLine("type is a shell builtin");
+    else if(command.Contains("type type"))
+        Console.WriteLine("type is a shell builtin");
+    else
+    {
+        var pathArray = GetPath();
 
-else if(command.Contains("type"))
-{
+        bool foundFlag = false;
 
-    var pathVar=Environment.GetEnvironmentVariable("PATH");
-    var pathArray=pathVar.Split(Path.PathSeparator);
-
-    bool foundFlag=false;
-
-        foreach (var address in pathArray)
+        foreach (var path in pathArray)
         {
-            var wholePath = Path.Join(address, command.Replace("type", "").Trim());
+            var wholePath = Path.Join(path, command.Replace("type", "").Trim());
             if (File.Exists(wholePath))
             {
                 Console.WriteLine($"{command.Replace("type", "").Trim()} is {wholePath}");
@@ -41,18 +116,16 @@ else if(command.Contains("type"))
                 break;
             }
         }
-
         if (foundFlag == false)
-        {
             Console.WriteLine($"{command.Replace("type", "").Trim()}: not found");
-        }
-
     }
+}
 
-else if(command.Contains("echo"))
-    Console.WriteLine($"{command.Replace("echo","").Trim()}");
-else
-    Console.WriteLine($"{command}: command not found");
-// Wait for user input
-//Console.ReadLine();
+static string[] GetPath()
+{
+    var pathVar = Environment.GetEnvironmentVariable("PATH");
+    var pathArray = pathVar.Split(Path.PathSeparator);
+
+    return pathArray;
+
 }
